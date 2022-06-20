1969276df4218e66ba50dc4c407e491a637f1e29
Mtsumi <mtsumijr@gmail.com>
2022-06-20 18:58:54 +0000

    exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except Exception as err:
-            print(err)
+        except IndexError:
             pass
     print()
     return count
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception as err:
-        print(err)
+    except:
         return False
