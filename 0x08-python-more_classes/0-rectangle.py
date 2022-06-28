387b3fd5a2765f202d8db5d5ba719e0587a4878c
Mtsumi <mtsumijr@gmail.com>
2022-06-28 15:06:54 +0000

    pycode style

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" This module defines a class 'Rectangle' """
+"""Module 0-rectangle
+Defines an empty Rectangle class.
+"""
 
 
 class Rectangle:
-    """ An empty class 'Rectangle'. """
+    """Empty class"""
     pass
