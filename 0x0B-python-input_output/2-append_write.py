f1c773d56cf83b00d8d5136eeee2010869e3bc1c
Mtsumi <mtsumijr@gmail.com>
2022-07-05 17:38:14 +0000

    changing function name

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """ Provides a function to append to files """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ Appends text to a file passed as an argument"""
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
