10f511db5189865e7dd99d80bd64b5f1d8654b57
Mtsumi <mtsumijr@gmail.com>
2022-07-05 17:19:16 +0000

    correcting space errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,12 +5,12 @@ Reads from a file and prints it t stdout.
 
 
 def read_file(filename=""):
-    """Reads from filename 
+    """Reads from filename
     and prints its contents to stdout.
     Args:
         - filename: name of the file
     """
 
-    with open(filename, encoding = "utf=8") as f:
+    with open(filename, encoding="utf=8") as f:
         read_text = f.read()
         print(read_text, end="")
