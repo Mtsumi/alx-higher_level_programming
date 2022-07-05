bffdf09127441249a4e3d1fef668ed10f8a4f231
Mtsumi <mtsumijr@gmail.com>
2022-07-05 17:26:53 +0000

    removing encoding arg

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,9 +8,9 @@ def read_file(filename=""):
     """Reads from filename
     and prints its contents to stdout.
     Args:
-        - filename: name of the file
+        - filename: name of the file to be read
     """
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename) as f:
         read_text = f.read()
         print(read_text, end="")
