d857a157da0b1563b418dde91550c5cfa88eb7f8
Mtsumi <mtsumijr@gmail.com>
2022-07-05 17:36:12 +0000

    append function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""Module 0-read_file.
-Reads from a file and prints it to stdout.
-"""
+"""Reads from a file and prints it to stdout."""
 
 
 def read_file(filename=""):
-    """Reads file and prints its contents to stdout.
-    Args: filename: name of the file to be read
-    """
+    """Reads file and prints its contents to stdout."""
     with open(filename) as f:
         read_text = f.read()
         print(read_text, end="")
