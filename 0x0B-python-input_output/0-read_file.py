ddc531845753a999c24970530914a2b718aaf933
Mtsumi <mtsumijr@gmail.com>
2022-07-05 17:32:46 +0000

    1 and 2 solutions

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """Module 0-read_file.
-Reads from a file and prints it t stdout.
+Reads from a file and prints it to stdout.
 """
 
 
 def read_file(filename=""):
-    """Reads from filename
-    and prints its contents to stdout.
-    Args:
-        - filename: name of the file to be read
+    """Reads file and prints its contents to stdout.
+    Args: filename: name of the file to be read
     """
-
     with open(filename) as f:
         read_text = f.read()
         print(read_text, end="")
