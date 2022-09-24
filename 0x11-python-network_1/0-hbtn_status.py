02f22ce32ff08c1f0b06f4f43a84c7de593a59a2
Mtsumi <mtsumijr@gmail.com>
2022-09-24 12:58:37 +0300

    editing readme

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,8 @@ using urlib package
 
 import urllib.request
 
-if __name__ == '__main__':
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
+if __name__ == "__main__":
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as r:
         res = r.read()
         print("Body response:")
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # alx-higher_level_programming
 ## This repository includes different tasks on the Python Programming language as part of the Software Engineerring programme at ALX.
 
+0x11. Python - Network #1: Basics of networking using urllib package to read websites using python
